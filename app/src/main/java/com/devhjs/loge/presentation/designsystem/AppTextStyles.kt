34c9beb9c0b1b826543d8ae6be9752e8bf3a34d6
Heo Junseo <107256859+Junseo0324@users.noreply.github.com>
2026-02-07 12:04:18 +0900

    design: 텍스트 스타일(AppTextStyles) 폰트 굵기 수정

--- app/src/main/java/com/devhjs/loge/presentation/designsystem/AppTextStyles.kt
+++ app/src/main/java/com/devhjs/loge/presentation/designsystem/AppTextStyles.kt
@@ -9,31 +9,31 @@ object AppTextStyles {
     object Pretendard {
         val Header1 = TextStyle(
             fontFamily = AppFonts.pretendard,
-            fontWeight = FontWeight.Medium,
+            fontWeight = FontWeight.SemiBold,
             fontSize = 24.sp,
             lineHeight = 36.sp
         )
         val Header2 = TextStyle(
             fontFamily = AppFonts.pretendard,
-            fontWeight = FontWeight.Medium,
+            fontWeight = FontWeight.SemiBold,
             fontSize = 20.sp,
             lineHeight = 30.sp
         )
         val Header3 = TextStyle(
             fontFamily = AppFonts.pretendard,
-            fontWeight = FontWeight.Medium,
+            fontWeight = FontWeight.Normal,
             fontSize = 18.sp,
             lineHeight = 27.sp
         )
         val Header4 = TextStyle(
             fontFamily = AppFonts.pretendard,
-            fontWeight = FontWeight.Medium,
+            fontWeight = FontWeight.Normal,
             fontSize = 16.sp,
             lineHeight = 24.sp
         )
         val Header5 = TextStyle(
             fontFamily = AppFonts.pretendard,
-            fontWeight = FontWeight.Medium,
+            fontWeight = FontWeight.Normal,
             fontSize = 14.sp,
             lineHeight = 20.sp
         )
@@ -45,7 +45,7 @@ object AppTextStyles {
         )
         val Label = TextStyle(
             fontFamily = AppFonts.pretendard,
-            fontWeight = FontWeight.Medium,
+            fontWeight = FontWeight.Normal,
             fontSize = 16.sp,
             lineHeight = 24.sp
         )
