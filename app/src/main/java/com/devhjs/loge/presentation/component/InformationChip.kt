d04231e7b9d430f33671d6fbb3abc351024ad192
Heo Junseo <107256859+Junseo0324@users.noreply.github.com>
2026-02-07 12:04:27 +0900

    feat: InformationChip 컴포넌트 및 시간 아이콘 에셋 추가
    - 정보 표시를 위한 공통 UI 컴포넌트인 `InformationChip` 구현
    - `InformationChip`에서 사용할 시간 표시용 아이콘 에셋(`ic_time.xml`) 추가

---
+++ app/src/main/java/com/devhjs/loge/presentation/component/InformationChip.kt
@@ -0,0 +1,54 @@
+package com.devhjs.loge.presentation.component
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.devhjs.loge.R
+import com.devhjs.loge.presentation.designsystem.AppColors
+import com.devhjs.loge.presentation.designsystem.AppTextStyles
+
+@Composable
+fun InformationChip(
+    modifier: Modifier = Modifier,
+    icon: Int,
+    text: String,
+) {
+    Row(
+        modifier = modifier,
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        Icon(
+            modifier = Modifier.size(16.dp),
+            painter = painterResource(icon),
+            tint = AppColors.iconPrimary,
+            contentDescription = text,
+        )
+        Spacer(modifier = Modifier.width(8.dp))
+
+        Text(
+            text = text,
+            style = AppTextStyles.Pretendard.Label.copy(
+                color = AppColors.labelTextColor
+            )
+        )
+        Spacer(modifier = Modifier.width(16.dp))
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun InformationChipPreview() {
+    InformationChip(
+        icon = R.drawable.ic_date,
+        text = "2026.02.04(수)"
+    )
+}
