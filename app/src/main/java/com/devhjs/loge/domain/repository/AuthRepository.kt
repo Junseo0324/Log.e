9b1255cd6805901dfad34e4e0e6c5741f0dc8535
Heo Junseo <107256859+Junseo0324@users.noreply.github.com>
2026-02-16 02:06:36 +0900

    feat: AuthRepository 내 세션 상태 확인 기능 추가
    - 사용자의 로그인 및 로그아웃 상태를 실시간으로 관찰하기 위해 `getSessionStatus` 메서드를 `AuthRepository` 인터페이스에 추가
    - 해당 메서드는 `Flow<Boolean>` 타입을 반환하여 세션 상태 변화를 비동기적으로 감지할 수 있도록 함

--- app/src/main/java/com/devhjs/loge/domain/repository/AuthRepository.kt
+++ app/src/main/java/com/devhjs/loge/domain/repository/AuthRepository.kt
@@ -1,6 +1,9 @@
 package com.devhjs.loge.domain.repository
 
+import kotlinx.coroutines.flow.Flow
+
 interface AuthRepository {
+    fun getSessionStatus(): Flow<Boolean> // true: 로그인 됨, false: 로그아웃 됨
     suspend fun signInWithGithub()
     suspend fun signOut()
     fun isUserLoggedIn(): Boolean
